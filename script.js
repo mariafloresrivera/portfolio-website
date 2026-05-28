cd88635b83486eed32f6e1348fabdd41be036b20
mariafloresrivera <mariafloresrivera.mfr@gmail.com>
2026-05-28 15:05:57 -0700

    Added projects, picture, and updated resume

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-document.write("Hello, World!<br>");
+function intro(){
+    document.write("Hello, I am a recent graduate from Sacramento State University. As an emerging professional, I am interested in dabbling in all areas of IT to find my niche. Most recently, I have been working on developing my skills in software development and cybersecurity. ");
+}
+
+function project(name, techs, desc){
+
+    const newDiv = document.createElement("div");
+    newDiv.className = "Project";
+
+    const title = document.createElement('h2');
+    const cont = document.createTextNode(name + " | " + techs);
+
+    const descCont = document.createElement("p");
+    const descText = document.createTextNode(desc);
+    title.appendChild(cont);
+    descCont.appendChild(descText);
+
+
+    newDiv.appendChild(title);
+    newDiv.appendChild(descCont);
+    document.body.appendChild(newDiv);
+
+}
+    
